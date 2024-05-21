0c453604dc6ef395cc341fd0ceac066450d4ea5e
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-22 01:45:17 +0530

    Update main.py

--- nandha/plugins/main.py
+++ nandha/plugins/main.py
@@ -10,7 +10,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     user_id = update.effective_user.id
     user_name = update.effective_user.first_name
-    mention = helpers.escape_markdown(helpers.mention_markdown(user_id, user_name))
+    mention = helpers.mention_markdown(user_id=user_id, name=user_name, version=2)
 
     keyboard = [
         [
@@ -26,7 +26,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text=(f"*Hello there {mention}, I'm Simple bot made by @NandhaBots using [Python Telegram Bot](https://docs.python-telegram-bot.org) Library.*"),
+        text=(f"*Hello there {mention}, I'm Simple bot made by @NandhaBots using [PythonTelegramBot](https://docs.python-telegram-bot.org) Library.*"),
         parse_mode=constants.ParseMode.MARKDOWN_V2,
         reply_markup=buttons)
 
