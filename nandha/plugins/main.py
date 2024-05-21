3c76723b63b2552de9e99b1587698d5e3947f4b6
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-22 01:34:16 +0530

    Update main.py

--- nandha/plugins/main.py
+++ nandha/plugins/main.py
@@ -2,7 +2,7 @@
 
 # nandha/plugins/main.py
 
-from telegram import Update, constants
+from telegram import Update, constants, helpers, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CommandHandler, ContextTypes
 from nandha import app
 
@@ -10,12 +10,25 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     user_id = update.effective_user.id
     user_name = update.effective_user.first_name
-    mention = f'[{user_name}](tg://user?id={user_id})'
+    mention = helpers.mention_markdown(user_id, user_name)
 
+    keyboard = [
+        [
+            InlineKeyboardButton("Group 🌟", url="NandhaChat.t.me"),
+            InlineKeyboardButton("Channel 💫", url="NandhaBots.t.me"),
+        ],
+        [
+            InlineKeyboardButton("Nandha 💀", url=f"tg://user?id={user_id}")
+        ]
+    ]
+
+    buttons = InlineKeyboardMarkup(keyboard)
+    
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=(f"*Hello there {mention}, I'm Simple bot made by @NandhaBots using [Python Telegram Bot](https://docs.python-telegram-bot.org) Library.*"),
-        parse_mode=constants.ParseMode.MARKDOWN)
+        parse_mode=constants.ParseMode.MARKDOWN,
+        reply_markup=buttons)
 
 start_handler = CommandHandler('start', start)
 app.add_handler(start_handler)
