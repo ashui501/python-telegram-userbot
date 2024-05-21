ca13937e931dedd7e253dc087921c2e5f8b1ef38
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-21 22:35:28 +0530

    Update main.py

--- nandha/plugins/main.py
+++ nandha/plugins/main.py
@@ -1,16 +1,16 @@
 
 
-from nandha import app
-from telegram.ext import CommandHandler
+# nandha/plugins/main.py
 
+from telegram import Update
+from telegram.ext import CommandHandler, ContextTypes
+from nandha import app
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text="I'm a bot, please talk to me!"
     )
-
-
 
 start_handler = CommandHandler('start', start)
 app.add_handler(start_handler)
