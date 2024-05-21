11a1a21844e5685f0cf92bed75d7922a0983d74c
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-22 01:03:48 +0530

    Update __main__.py

--- nandha/__main__.py
+++ nandha/__main__.py
@@ -1,11 +1,14 @@
+import importlib
+import os
+import pkgutil
 
-from nandha import app
-
-from nandha.plugins import (
-main
-)
-
-
+def import_plugins(package):
+    package_dir = os.path.dirname(package.__file__)
+    for _, module_name, _ in pkgutil.iter_modules([package_dir]):
+        importlib.import_module(f"{package.__name__}.{module_name}")
 
 if __name__ == '__main__':
+    from nandha import app
+    from nandha import plugins  # Assuming plugins is a package with __init__.py
+    import_plugins(plugins)
     app.run_polling()
