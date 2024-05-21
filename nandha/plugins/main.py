c49b273db7d3ed24e1d5aed0bfd321bf2ca54838
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-05-22 00:48:52 +0530

    Update main.py

--- nandha/plugins/main.py
+++ nandha/plugins/main.py
@@ -2,7 +2,7 @@
 
 # nandha/plugins/main.py
 
-from telegram import Update
+from telegram import Update, constants
 from telegram.ext import CommandHandler, ContextTypes
 from nandha import app
 
@@ -12,10 +12,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_name = update.effective_user.first_name
     mention = f'[{user_name}](tg://user?id={user_id})'
 
-
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text=(f"**Hello there {mention}, I'm simple bot made by @NandhaBots using [Python-Telegram-Bot](https://docs.python-telegram-bot.org) Library.**"))
+        text=(f"**Hello there {mention}, I'm Simple bot made by @NandhaBots using [Python-Telegram-Bot](https://docs.python-telegram-bot.org) Library.**"),
+        constants.ParseMode.MARKDOWN_V2)
 
 start_handler = CommandHandler('start', start)
 app.add_handler(start_handler)
